215346e9075657ebccba1c86dc00a313dfe1185c
Anamika dobriyal <anamikadobriyal21@gmail.com>
2024-11-28 22:59:22 +0530

    first React assignment solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState }  from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  // State to track the button click count
+  const [count, setCount] = useState(0);
+
+  // Event handler to increment the count
+  const handleButtonClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times.</p>
+      <button onClick={handleButtonClick}>Click Me</button>
+
     </div>
   )
 }
